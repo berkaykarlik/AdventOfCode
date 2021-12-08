82c00469974b40fe2a29038c2e69b68481b68394
berkaykarlik <brkykrlk@gmail.com>
2021-12-08 22:14:02 +0300

    ADD: day8 part 2 completed

--- day8/segment.py
+++ day8/segment.py
@@ -1,45 +1,35 @@
 from pathlib import Path
 from collections import namedtuple
-input_file_path = Path("test.txt")
-input_str = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
+input_file_path = Path("input.txt")
+input_str = input_file_path.read_text()
 entries_str = input_str.split("\n")
 Entry = namedtuple('Entry', 'signal output')
-entries = list(map(lambda x:x.split(" | "),entries_str))
-entries = list(map(lambda x: Entry(x[0].split(" "),x[1].split(" ")),entries))
+entries = list(map(lambda x: x.split(" | "), entries_str))
+entries = list(map(lambda x: Entry(x[0].split(" "), x[1].split(" ")), entries))
 
 len2digit = {
-    2:1,
-    4:4,
-    3:7,
-    7:8
+    2: 1,
+    4: 4,
+    3: 7,
+    7: 8
 }
 
-#------ Part 1 -----
+# ------ Part 1 -----
+
+
 def count_unique():
     count_1_4_7_8 = 0
     for entry in entries:
         for output in entry.output:
             if len2digit.get(len(output)):
-                count_1_4_7_8 +=1
+                count_1_4_7_8 += 1
     return count_1_4_7_8
 
-print("count_1_4_7_8: ",count_unique()) 
+
+print("count_1_4_7_8: ", count_unique())
 # ---------
 
-digit2signal = {
-    0: "abcefg",
-    1: "cf",
-    2: "acdeg",
-    3: "acdfg",
-    4: "bcdf",
-    5: "abdfg",
-    6: "abdefg",
-    7: "acf",
-    8: "abcdefg",
-    9: "abcdfg"    
-}
-
-
+sum = 0
 for entry in entries:
     numbers_found = [None for i in range(10)]
     two_three_five = []
@@ -59,8 +49,22 @@ for entry in entries:
     three_horizontal_lines = set.intersection(*two_three_five)
     numbers_found[9] = numbers_found[4].union(three_horizontal_lines)
     left_bottom = numbers_found[8] - numbers_found[9]
-    numbers_found[6] = set.union(left_top_right_bottom_edge,three_horizontal_lines,left_bottom)
-    numbers_found[0] = list(filter(lambda x: x != numbers_found[6] and x != numbers_found[9],zero_six_nine))[0]
+    numbers_found[6] = set.union(
+        left_top_right_bottom_edge, three_horizontal_lines, left_bottom)
+    numbers_found[0] = list(filter(
+        lambda x: x != numbers_found[6] and x != numbers_found[9], zero_six_nine))[0]
     numbers_found[5] = numbers_found[9].intersection(numbers_found[6])
-    numbers_found[2] = (numbers_found[0]-numbers_found[5]).union(three_horizontal_lines)
-    print(numbers_found[2])
+    numbers_found[2] = (numbers_found[0]-numbers_found[5]
+                        ).union(three_horizontal_lines)
+    numbers_found[3] = three_horizontal_lines.union(numbers_found[1])
+
+    output_number = 0
+    for j, output in enumerate(entry.output):
+        for i, number in enumerate(numbers_found):
+            if len(output) == len(number) and sorted(output) == sorted(number):
+                output_number += i * 10**(3-j)
+                break
+    sum += output_number
+
+
+print(sum)
