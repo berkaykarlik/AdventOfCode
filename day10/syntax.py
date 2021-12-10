8c18312888cee22974f5c721deb22948f33c63bf
berkayKarlik <brkykrlk@gmail.com>
2021-12-10 13:39:25 +0300

    day 10 part 2

--- day10/syntax.py
+++ day10/syntax.py
@@ -26,11 +26,40 @@ def is_complete(line):
             matching_parenthesis = stack.pop()
             if chunks[matching_parenthesis] != parenthesis:
                 illegal = parenthesis
-                return False ,illegal
-    return True, illegal
+                return False ,illegal, None
+
+    if not stack:
+        return True, illegal, None
+    
+    completor = []
+    for i in range(len(stack)):
+        incomplete = stack.pop()
+        completor.append(chunks[incomplete])
+    return True, illegal, completor
+    
+
 
 result_list = list(map(is_complete,input_list))
 wrong_ones = list(filter(lambda x: x[0]== False,result_list))
-get_line_score =  lambda illegals :sum(map(closing_score.get,illegals))
+get_line_score =  lambda arr :sum(map(closing_score.get,arr))
 score = sum(map(lambda x: get_line_score(x[1]),wrong_ones))
 print(score)
+
+completion_score = {
+    ")":1,
+    "]":2,
+    "}":3,
+    ">":4
+}
+
+missing_ones = list(filter(lambda x: x[0]== True,result_list))
+
+def get_complation_score(completors):
+    scores = map(completion_score.get,completors)
+    score = 0
+    for i,s in enumerate(scores):
+        score = score * 5 + s
+    return score
+
+scores = list(map(lambda x: get_complation_score(x[2]),missing_ones))
+print(sorted(scores)[len(scores)//2 ])
