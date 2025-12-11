a22bc7a465b7e915f0c26d0fb137f5c471efa6c2
berkaykarlik <brkykrlk@gmail.com>
2025-12-11 23:44:54 +0300

    ADD: partday5

--- 2025/day5/day5.c
+++ 2025/day5/day5.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 typedef struct{
-    int start;
-    int end;
+    long long start;
+    long long end;
 }Range;
 
 //dont forget to dealloc ranges and ids
-void parse_file(char* filename, Range **ranges_p, int **ids_p, int *ranges_size, int *ids_size){
+void parse_file(char* filename, Range **ranges_p, long long **ids_p, int *ranges_size, int *ids_size){
 
     int ranges_buffer_size = 2;
     *ranges_p = (Range*)malloc(ranges_buffer_size* sizeof(Range));
@@ -16,11 +16,11 @@ void parse_file(char* filename, Range **ranges_p, int **ids_p, int *ranges_size,
     FILE *fptr;
     fptr = fopen(filename, "r");
 
-    int start = 0;
-    int end = 0;
-    printf("reading ranges\n");
-    while (fscanf(fptr, "%d-%d", &start, &end) == 2) {
-        printf("start: %d\t end: %d\n", start, end);
+    long long start = 0;
+    long long end = 0;
+    // printf("reading ranges\n");
+    while (fscanf(fptr, "%lld-%lld", &start, &end) == 2) {
+        // printf("start: %d\t end: %d\n", start, end);
 
         if((ranges_buffer_size - read_index) <= 0){
             ranges_buffer_size *= 2;
@@ -38,17 +38,17 @@ void parse_file(char* filename, Range **ranges_p, int **ids_p, int *ranges_size,
    *ranges_size = read_index;
 
    int ids_buffer_size = 2;
-   *ids_p = (int*)malloc(sizeof(int)* ids_buffer_size);
+   *ids_p = (long long*)malloc(sizeof(long long)* ids_buffer_size);
    read_index = 0;
 
-   int current_id = 0;
-    printf("reading ids\n");
-    while (fscanf(fptr, "%d", &current_id) == 1) {
-        printf("id:\t%d\n", current_id);
+   long long current_id = 0;
+    // printf("reading ids\n");
+    while (fscanf(fptr, "%lld", &current_id) == 1) {
+        // printf("id:\t%d\n", current_id);
 
-        if((ranges_buffer_size - read_index) <= 0){
+        if((ids_buffer_size - read_index) <= 0){
             ids_buffer_size *= 2;
-            int* temp = realloc(*ids_p,sizeof(int) * ids_buffer_size);
+            long long* temp = realloc(*ids_p,sizeof(long long) * ids_buffer_size);
             if (temp == NULL) {
                 printf("realloc failed, abondoning all hope...\n");
                 free(*ranges_p);
@@ -64,25 +64,39 @@ void parse_file(char* filename, Range **ranges_p, int **ids_p, int *ranges_size,
    fclose(fptr);
 }
 
+int is_in_range(long long i, Range r){
+    if ((i >= r.start) && (i <= r.end))
+        return 1;
+    return 0;
+}
+
 int main(int argc, char** argv){
 
-    int *ids;
+    long long *ids;
     Range *ranges;
     int ranges_size, ids_size;
+    int fresh_ones = 0;
 
     parse_file(argv[1],&ranges,&ids, &ranges_size, &ids_size);
-    printf("ranges_size %d \n", ranges_size);
+    // printf("ranges_size %d \n", ranges_size);
 
-    // verifying range parsing
-    for (int i = 0; i < ranges_size; i ++){
-        printf("start: %d \tend %d\n", ranges[i].start, ranges[i].end);
-    }
+    // // verifying range parsing
+    // for (int i = 0; i < ranges_size; i ++){
+    //     printf("start: %d \tend %d\n", ranges[i].start, ranges[i].end);
+    // }
     //verfying ids parsing
     for (int i = 0; i < ids_size; i++){
-        printf("id%d:\t %d\n",i, ids[i]);
+        // printf("id%d:\t %d\n",i, ids[i]);
+        for (int j = 0; j < ranges_size; j ++){
+            Range r = ranges[j];
+            if (is_in_range(ids[i],r)){
+                fresh_ones ++;
+                break;
+            }
+        }
     }
 
-    //Answer goes here
+    printf("Total fresh count %d\n",fresh_ones);
 
     free(ranges);
     free(ids);
